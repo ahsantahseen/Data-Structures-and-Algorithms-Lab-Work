7bec5012f1998ee280c4b7860d23ccdffbb69f65
Ahsan Tahseen <58149036+ahsantahseen@users.noreply.github.com>
2020-10-12 14:25:31 +0500

    Adding stack methods

--- stackClass.java
+++ stackClass.java
@@ -2,6 +2,18 @@ class StackDS {
     private int maxLen;
     private int[] stackArray;
     private int top;
+
+    public StackDS(int size) {
+        this.maxLen = size;
+        this.stackArray = new int[size];
+        this.top = -1;// because when we add it will be 0 and array starting element index is 0
+
+    }
+
+    public void push(int j) {
+        top++;
+        stackArray[top] = j;
+    }
 }
 
 public class stackClass {
