8e74c93298e83fbc7b5f29fa990727ced7ea421e
Ahsan Tahseen <58149036+ahsantahseen@users.noreply.github.com>
2020-10-26 14:58:55 +0500

    Circular Queue

--- circularQueue.java
+++ circularQueue.java
@@ -30,16 +30,23 @@ public class circularQueue {
         }
     }
 
-    public void Enqueue() {
+    public void Enqueue(Employee employee) {
         if (size() == queueArray.length - 1) {
             int numOfitems = size();
             Employee[] newArray = new Employee[queueArray.length * 2];
-            //It will copy from front to the last element from the orignal queue//
+            // It will copy from front to the last element from the orignal queue//
             System.arraycopy(queueArray, FrontPointer, newArray, 0, queueArray.length - FrontPointer);
-            //It will copy from 0 to the back pointer from the orignal queue//
-            System.arraycopy(queueArray, 0, newArray, queueArray.length-FrontPointer, BackPointer);
-            FrontPointer=0;
-            BackPointer=numOfitems;
+            // It will copy from 0 to the back pointer from the orignal queue//
+            System.arraycopy(queueArray, 0, newArray, queueArray.length - FrontPointer, BackPointer);
+            FrontPointer = 0;
+            BackPointer = numOfitems;
+        }
+        queueArray[BackPointer] = employee;
+        if (BackPointer < queueArray.length - 1) {
+        BackPointer++;
+        }
+        else{
+            BackPointer=0;
         }
     }
 }
