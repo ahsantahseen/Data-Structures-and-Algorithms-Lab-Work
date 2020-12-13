f4b2f72cbf3e7119d7e1bc1f7d60bf67d1e96c96
Ahsan Tahseen <58149036+ahsantahseen@users.noreply.github.com>
2020-12-13 20:56:56 +0500

    Insertion Sort

--- sorting.java
+++ sorting.java
@@ -2,8 +2,10 @@ public class sorting {
 
     public static void main(String[] args) {
         int[] items = new int[] { 13, 2, 133, 4, 140, 12, 214 };
+        int[] items1 = new int[] { 313, 25, 133, 14, 10, 12, 214 };
         sorting obj = new sorting();
         obj.SelectionSort(items);
+        obj.InsertionSort(items1);
 
     }
 
@@ -24,6 +26,29 @@ public class sorting {
             a[i] = a[smallestElementIndex];
             a[smallestElementIndex] = temp;
 
+        }
+        System.out.println();
+        System.out.println("SORTED");
+        for (int i = 0; i < a.length; i++) {
+            System.out.print("\t" + a[i]);
+        }
+    }
+
+    public void InsertionSort(int a[]) {
+        System.out.println();
+        System.out.println("UNSORTED");
+        for (int i = 0; i < a.length; i++) {
+            System.out.print("\t" + a[i]);
+        }
+        int key, value;
+        for (int i = 1; i < a.length; i++) {
+            key = i;
+            value = a[i];
+            while (key > 0 && value < a[key - 1]) {
+                a[key] = a[key - 1];
+                key--;
+            }
+            a[key] = value;
         }
         System.out.println();
         System.out.println("SORTED");
