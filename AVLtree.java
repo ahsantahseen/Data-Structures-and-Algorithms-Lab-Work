631c51d03fdd917a06dd60f9182bb2e1772de41a
Ahsan Tahseen <58149036+ahsantahseen@users.noreply.github.com>
2021-01-17 04:17:31 +0500

    AVL Tree

--- AVLtree.java
+++ AVLtree.java
@@ -41,11 +41,10 @@ public class AVLtree {
         }
         root.height = Math.max(height(root.leftchild), height(root.rightchild)) + 1;
         var BalanceFactor = BalanceFactor(root);
-        if (isLeftHeavy(root)) {
-            System.out.println(root.value + " is left heavy");
-        } else if (isRightHeavy(root)) {
-            System.out.println(root.value + " is right heavy");
-        }
+
+        setHeight(root);
+        root = balance(root);
+
         return root;
     }
 
@@ -70,11 +69,61 @@ public class AVLtree {
         return (node == null) ? 0 : height(node.leftchild) - height(node.rightchild);
     }
 
+    // Function to balance the tree
+
+    // Function to balance the tree
+
+    private AVLNode balance(AVLNode root) {
+        if (isLeftHeavy(root)) {
+            if (BalanceFactor(root.leftchild) < 0) {
+                root.leftchild = rotateLeft(root.leftchild); // Perfom First left then Right Rotate in the next line if
+                                                             // condition is true
+            }
+            return rotateRight(root); // return new node after Right rotate only if the above conditon is false
+
+        } else if (isRightHeavy(root)) {
+            if (BalanceFactor(root.rightchild) > 0) {
+                root.rightchild = rotateRight(root.rightchild); // Perform First Right then left rotate in the next line
+                                                                // if the conidtion is true
+            }
+            return rotateLeft(root); // return new node after Left rotate only if the above conditon is false
+        }
+        return root;
+
+    }
+
+    // Rotation Function
+
+    private AVLNode rotateLeft(AVLNode root) {
+        var newRoot = root.rightchild;
+        root.leftchild = newRoot.rightchild;
+        newRoot.leftchild = root;
+        setHeight(root);
+        setHeight(newRoot);
+        return newRoot;
+    }
+
+    private AVLNode rotateRight(AVLNode root) {
+        var newRoot = root.leftchild;
+        root.rightchild = newRoot.leftchild;
+        newRoot.rightchild = root;
+        setHeight(root);
+        setHeight(newRoot);
+        return newRoot;
+
+    }
+
+    private void setHeight(AVLNode node) {
+        node.height = Math.max(height(root.leftchild), height(root.rightchild)) + 1;
+    }
+
     public static void main(String[] args) {
         AVLtree AVLtree = new AVLtree();
+        AVLtree.Insert(30);
+        AVLtree.Insert(20);
         AVLtree.Insert(10);
-        AVLtree.Insert(8);
-        AVLtree.Insert(11);
-        System.out.println(AVLtree.height(AVLtree.root));
+        AVLtree.balance(AVLtree.root);
+        System.out.println("IS LEFT HEAVY? " + AVLtree.isLeftHeavy(AVLtree.root));
+        System.err.println("IS RIGHT HEAVY? " + AVLtree.isRightHeavy(AVLtree.root));
     }
 }
